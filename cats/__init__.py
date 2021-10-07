5775dde466fdff5a08b280d0220316418aef0b83
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-10-07 18:54:28 +0530

    Format docstrings

--- cats/__init__.py
+++ cats/__init__.py
@@ -1,5 +1,7 @@
 """
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 An API wrapper for thecatapi.com.
 
-Supports asynchronous applications (use AsyncClient) class) and sync applic
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
