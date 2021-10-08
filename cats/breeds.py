7f5a2d0fa9e245220c84369280cb642ebd00d20b
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-10-08 16:12:56 +0530

    Support for parameters in Client.get_breeds

--- cats/breeds.py
+++ cats/breeds.py
@@ -4,9 +4,16 @@ from urllib.parse import quote
 
 class BreedsMixin(BaseMixin):
 
-    def get_breeds(self):
-        ses = self.session
-        s = ses.get(f"{self.BASE}/breeds")
+    def get_breeds(self, *, attach_breed: int = 0, page: int = None, limit: int = None):
+        p = {
+            "attach_breed": attach_breed,
+        }
+        if page is not None:
+            p["page"] = page
+        if limit is not None:
+            p["limit"] = limit
+
+        s = self.session.get(f"{self.BASE}/breeds", params=p)
         json = s.json()
         return [Breed(**data) for data in json]
 
