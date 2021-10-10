1c8e5a7cae2994ac89fbd696904a25584e6edd3d
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-10-10 19:21:43 +0530

    Seperate HTTPClient class for making requests

---
+++ cats/http.py
@@ -0,0 +1,27 @@
+from requests import Session, Response
+from .utils import _raise_for_status
+
+
+
+class HTTPClient(Session):
+
+    def __init__(self, api_key) -> None:
+        self.headers["x-api-key"] = api_key
+
+    def post(self, url, data=None, json=None, **kwargs) -> Response:
+        res = super().post(url, data=data, json=json, **kwargs)
+        s = _raise_for_status(res.status_code)
+        if s:
+            return res
+
+    def get(self, url, **kwargs) -> Response:
+        res = super().get(url, **kwargs)
+        s = _raise_for_status(res.status_code)
+        if s:
+            return res
+
+    def delete(self, url, **kwargs) -> Response:
+        res = super().delete(url, **kwargs)
+        s = _raise_for_status(res.status_code)
+        if s:
+            return res
