c92a7cbc57ffb65d20aa5db6a4bf0d3d6b192006
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-10-11 19:55:21 +0530

    Create examples for ease of usage

---
+++ examples/breeds.py
@@ -0,0 +1,13 @@
+from cats import Client
+
+client = Client(api_key="API KEY HERE")
+
+all_breeds = client.get_breeds()
+# returns all the breeds available as a list of cats.Breed objects
+
+print(all_breeds[0].wikipedia_url)
+# prints the wikipedia URL of the first breed in the list
+
+beng = client.search_breed("beng") # short for bengal
+# beng is an instance of cat.Breed
+print(beng.wikipedia_url)
