e96ec463f963d5e24a64680436fa39ffd30154df
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-10-13 15:50:22 +0530

    Docstrings for FavouritesMixin

--- cats/mixins/favourites.py
+++ cats/mixins/favourites.py
@@ -6,6 +6,16 @@ class FavouritesMixin(BaseMixin):
     def get_all_favourites(
         self, *, sub_id: str = None, limit: str = None, page: str = None
     ):
+        """Get all of the favourites that belong to your account
+
+        Arguments:
+            ``sub_id`` (str, optional): For filtering. Defaults to None.
+            ``limit`` (str, optional): limit the amount of results. Defaults to None.
+            ``page`` (str, optional): For pagination. Defaults to None.
+
+        Returns:
+            ``List[cats.Favourite]``: Favourites belonging to your account
+        """
         url = f"{self.BASE}/favourites"
         query = _resolve_query(sub_id=sub_id, limit=limit, page=page)
         res = self.session.get(url, params=query)
@@ -13,6 +23,15 @@ class FavouritesMixin(BaseMixin):
         return [Favourite(**data) for data in json]
 
     def save_favourite(self, image_id: str, sub_id: str = None):
+        """Save an image as favourite
+
+        Arguments:
+            ``image_id`` (str): The id of the image to save as favourite
+            ``sub_id`` (str, optional): Defaults to None.
+
+        Returns:
+            ``cats.Response``: Response returned by the API, may contain unsuccesful response too
+        """
         body = _resolve_query(image_id=image_id, sub_id=sub_id)
         url = f"{self.BASE}/favourites"
         res = self.session.post(url, json=body)
@@ -20,12 +39,28 @@ class FavouritesMixin(BaseMixin):
         return Response(**json)
 
     def get_favourite(self, favourite_id: str):
+        """Get a specific favourite belonging to your account
+
+        Arguments:
+            ``favourite_id`` (str): ID of the favourite object. Return with the response when using ``cats.Client.save_favourite``
+
+        Returns:
+            ``cats.Favourite``: Information about the favourite returned by the API itself
+        """
         url = f"{self.BASE}/favourites/{favourite_id}"
         res = self.session.get(url)
         json = res.json()
         return Favourite(**json)
 
     def delete_favourite(self, favourite_id: str):
+        """Delete an image from your favourites
+
+        Arguments:
+            ``favourite_id`` (str): ID of the favourite object. Return with the response when using ``cats.Client.save_favourite``
+
+        Returns:
+            ``cats.Response``: Response returned by the API, may contain unsuccesful response too
+        """
         url = f"{self.BASE}/favourites/{favourite_id}"
         res = self.session.delete(url)
         json = res.json()
