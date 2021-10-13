71217cf3c9761a78b27310d8dfda7a9be43d2ac0
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-10-13 15:49:57 +0530

    Docstrings for CategoriesMixin

--- cats/mixins/categories.py
+++ cats/mixins/categories.py
@@ -4,6 +4,15 @@ from .base import BaseMixin
 
 class CategoriesMixin(BaseMixin):
     def get_categories(self, *, limit: int = None, page: int = None):
+        """Get all the categories available
+
+        Arguments:
+            ``limit`` (int, optional): Limit the amount of results to be returned. Defaults to None.
+            ``page`` (int, optional): For pagination. Defaults to None.
+
+        Returns:
+            ``List[cats.Category`` : A list of all the categories available
+        """
         query = _resolve_query(limit=limit, page=page)
         url = f"{self.BASE}/categories"
         res = self.session.get(url, params=query)
