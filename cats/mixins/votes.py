b3c6abd37f2bee98500e9b3a84af1a21de58be69
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-10-14 20:49:40 +0530

    Fix Client.vote_image parameters
    made sub_id optional for consistency with the API

--- cats/mixins/votes.py
+++ cats/mixins/votes.py
@@ -22,26 +22,22 @@ class VotesMixin(BaseMixin):
         json = res.json()
         return [Vote(**data) for data in json]
 
-    def vote_image(self, *, image_id: str, sub_id: str, value: int):
+    def vote_image(self, *, image_id: str, value: int, sub_id: str = None):
         """Upvote or Downvote an image
 
         Arguments:
             ``image_id`` (str): The ID of the image to vote
-            ``sub_id`` (str): Unique identification for you
+            ``sub_id`` (str, optional): Unique identification for you. Defaults to None
             ``value`` (int): 0 or 1 where 0 downvotes and 1 upvotes
 
         Returns:
             ``cats.Response``: Response returned by the API. may contain unsuccesful value
         """
-        assert isinstance(value, int) and value in [
+        assert isinstance(value, int) and value in {
             0,
             1,
-        ], "Value must be either 0 or 1"
-        body = {  # request body
-            "image_id": image_id,
-            "sub_id": sub_id,
-            "value": value,
-        }
+        }, "Value must be either 0 or 1"
+        body = _resolve_query(image_id=image_id, sub_id=sub_id, value=value)
         url = f"{self.BASE}/votes"
         res = self.session.post(url, json=body)
         json = res.json()
