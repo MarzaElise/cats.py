b408f3e89ec0887b3912c1c6523c227402a9135f
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-11-26 15:40:52 +0530

    Add meta variables

--- cats/__init__.py
+++ cats/__init__.py
@@ -8,3 +8,7 @@ An API wrapper for thecatapi.com.
 
 from .client import Client
 from .utils._dataclasses import *
+
+__author__ = "Marcus"
+__version__ = "1.0.0"
+__url__ = "https://github.com/MarzaElise/cats.py"
