ea9bd4481267dabc84738d8c6a66b8a866e865c7
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-12-17 00:01:57 +0530

    Fix dataclasses in async_cats

--- async_cats/utils/_dataclasses.py
+++ async_cats/utils/_dataclasses.py
@@ -1,140 +1,118 @@
-from dataclasses import dataclass
-from typing import Optional, TypeAlias, Union
-
-opt_str: TypeAlias = Optional[str]
-opt_int: TypeAlias = Optional[int]
+from pydantic import BaseModel
+from typing import Any, Optional, TypeAlias, Union
 
 
-@dataclass(frozen=True)
-class Breed:
+class Breed(BaseModel):
 
-    id: opt_str = None
-    name: opt_str = None
-    temperament: opt_str = None
-    life_span: opt_str = None
-    alt_names: opt_str = None
-    wikipedia_url: opt_str = None
-    origin: opt_str = None
-    weight_imperial: opt_str = None
-    country_codes: opt_str = None
-    country_code: opt_str = None
-    description: opt_str = None
-    weight: opt_str = None
-    cfa_url: opt_str = None
-    vetstreet_url: opt_str = None
-    vcahospitals_url: opt_str = None
-    reference_image_id: opt_str = None
-    lap: opt_str = None
-    image: opt_str = None
+    id: Optional[Union[str, Any]]
+    name: Optional[Union[str, Any]]
+    temperament: Optional[Union[str, Any]]
+    life_span: Optional[Union[str, Any]]
+    alt_names: Optional[Union[str, Any]]
+    wikipedia_url: Optional[Union[str, Any]]
+    origin: Optional[Union[str, Any]]
+    weight_imperial: Optional[Union[str, Any]]
+    country_codes: Optional[Union[str, Any]]
+    country_code: Optional[Union[str, Any]]
+    description: Optional[Union[str, Any]]
+    weight: Optional[Union[str, Any]]
+    cfa_url: Optional[Union[str, Any]]
+    vetstreet_url: Optional[Union[str, Any]]
+    vcahospitals_url: Optional[Union[str, Any]]
+    reference_image_id: Optional[Union[str, Any]]
+    lap: Optional[Union[str, Any]]
+    image: Optional[Union[str, Any]]
 
-    experimental: opt_int = None
-    suppressed_tail: opt_int = None
-    indoor: opt_int = None
-    hairless: opt_int = None
-    natural: opt_int = None
-    rare: opt_int = None
-    rex: opt_int = None
-    supress_tail: opt_int = None
-    short_legs: opt_int = None
-    hypoallergenic: opt_int = None
-    adaptability: opt_int = None
-    affection_level: opt_int = None
-    child_friendly: opt_int = None
-    dog_friendly: opt_int = None
-    stranger_friendly: opt_int = None
-    cat_friendly: opt_int = None
-    energy_level: opt_int = None
-    grooming: opt_int = None
-    health_issues: opt_int = None
-    intelligence: opt_int = None
-    shedding_level: opt_int = None
-    social_needs: opt_int = None
-    vocalisation: opt_int = None
-    bidability: opt_int = None
-
-
-@dataclass(frozen=True)
-class Category:
-
-    id: int = None
-    name: str = None
+    experimental: Optional[Union[int, Any]]
+    suppressed_tail: Optional[Union[int, Any]]
+    indoor: Optional[Union[int, Any]]
+    hairless: Optional[Union[int, Any]]
+    natural: Optional[Union[int, Any]]
+    rare: Optional[Union[int, Any]]
+    rex: Optional[Union[int, Any]]
+    supress_tail: Optional[Union[int, Any]]
+    short_legs: Optional[Union[int, Any]]
+    hypoallergenic: Optional[Union[int, Any]]
+    adaptability: Optional[Union[int, Any]]
+    affection_level: Optional[Union[int, Any]]
+    child_friendly: Optional[Union[int, Any]]
+    dog_friendly: Optional[Union[int, Any]]
+    stranger_friendly: Optional[Union[int, Any]]
+    cat_friendly: Optional[Union[int, Any]]
+    energy_level: Optional[Union[int, Any]]
+    grooming: Optional[Union[int, Any]]
+    health_issues: Optional[Union[int, Any]]
+    intelligence: Optional[Union[int, Any]]
+    shedding_level: Optional[Union[int, Any]]
+    social_needs: Optional[Union[int, Any]]
+    vocalisation: Optional[Union[int, Any]]
+    bidability: Optional[Union[int, Any]]
 
 
-@dataclass(frozen=True)
-class Vote:
+class Category(BaseModel):
 
-    id: str
-    image_id: str
-    sub_id: opt_str
-    created_at: str
-    value: int
-    country_code: opt_str = None
-    user_id: opt_str = None
+    id: Union[int, Any]
+    name: Union[str, Any]
 
 
-@dataclass(frozen=True)
-class Response:
+class Vote(BaseModel):
 
-    id: Union[str, int, None] = None
-    message: str = None
-    level: opt_str = None
-    status: opt_int = None
-
-
-@dataclass(frozen=True)
-class FavouriteImage:
-
-    id: str
-    url: str
+    id: Union[str, Any]
+    image_id: Union[str, Any]
+    sub_id: Optional[Union[str, Any]]
+    created_at: Union[str, Any]
+    value: Union[int, Any]
+    country_code: Optional[Union[str, Any]]
+    user_id: Optional[Union[str, Any]]
 
 
-@dataclass(frozen=True)
-class Favourite:
+class Response(BaseModel):
 
-    id: str
-    image_id: str
-    sub_id: str
-    created_at: str
+    id: Union[str, int, None]
+    message: Union[str, Any]
+    level: Optional[Union[str, Any]]
+    status: Optional[Union[int, Any]]
+
+
+class FavouriteImage(BaseModel):
+
+    id: Union[str, Any]
+    url: Union[str, Any]
+
+
+class Favourite(BaseModel):
+
+    id: Union[str, Any]
+    image_id: Union[str, Any]
+    sub_id: Union[str, Any]
+    created_at: Union[str, Any]
     image: FavouriteImage
-    user_id: str
+    user_id: Union[str, Any]
 
     def __post_init__(self):
         self.image = FavouriteImage(**self.image)
 
 
-@dataclass(frozen=True)
-class Image:
+class Image(BaseModel):
 
-    id: str = None
-    url: str = None
-    width: int = None
-    height: int = None
-    created_at: str = None
-    sub_id: str = None
-    original_filename: str = None
+    id: Union[str, Any]
+    url: Union[str, Any]
+    width: Union[int, Any]
+    height: Union[int, Any]
+    created_at: Union[str, Any]
+    sub_id: Union[str, Any]
+    original_filename: Union[str, Any]
 
-    breeds: Breed = None
-    categories: Category = None
-
-    def __post_init__(self):
-        if self.categories is None:
-            self.categories = []
-        if self.breeds is None:
-            self.breeds = []
-        self.breeds = [
-            Breed(**data) for data in self.breeds
-        ]  # api returns a list
-        self.categories = [
-            Category(**data) for data in self.categories
-        ]  # api returns list
+    breeds: Breed
+    categories: Category
 
 
-@dataclass(frozen=True)
-class Analysis:
 
-    image_id: str = None
-    labels: list[dict] = None
-    moderation_labels: list[dict] = None
-    vendor: str = None
-    approved: int = None
-    rejected: int = None
+class Analysis(BaseModel):
+
+    image_id: Union[str, Any]
+    labels: list[dict]
+    moderation_labels: list[dict]
+    vendor: Union[str, Any]
+    approved: Union[int, Any]
+    rejected: Union[int, Any]
