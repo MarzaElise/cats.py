17a9748d872eaed0bc1c056800229ab26c0d9825
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-12-17 00:00:13 +0530

    Type everything with Any due to API inconsistency

--- cats/utils/_dataclasses.py
+++ cats/utils/_dataclasses.py
@@ -1,96 +1,93 @@
 from pydantic import BaseModel
-from typing import Optional, TypeAlias, Union
-
-opt_str: TypeAlias = Optional[str]
-opt_int: TypeAlias = Optional[int]
+from typing import Any, Optional, TypeAlias, Union
 
 
 class Breed(BaseModel):
 
-    id: opt_str
-    name: opt_str
-    temperament: opt_str
-    life_span: opt_str
-    alt_names: opt_str
-    wikipedia_url: opt_str
-    origin: opt_str
-    weight_imperial: opt_str
-    country_codes: opt_str
-    country_code: opt_str
-    description: opt_str
-    weight: opt_str
-    cfa_url: opt_str
-    vetstreet_url: opt_str
-    vcahospitals_url: opt_str
-    reference_image_id: opt_str
-    lap: opt_str
-    image: opt_str
+    id: Optional[Union[str, Any]]
+    name: Optional[Union[str, Any]]
+    temperament: Optional[Union[str, Any]]
+    life_span: Optional[Union[str, Any]]
+    alt_names: Optional[Union[str, Any]]
+    wikipedia_url: Optional[Union[str, Any]]
+    origin: Optional[Union[str, Any]]
+    weight_imperial: Optional[Union[str, Any]]
+    country_codes: Optional[Union[str, Any]]
+    country_code: Optional[Union[str, Any]]
+    description: Optional[Union[str, Any]]
+    weight: Optional[Union[str, Any]]
+    cfa_url: Optional[Union[str, Any]]
+    vetstreet_url: Optional[Union[str, Any]]
+    vcahospitals_url: Optional[Union[str, Any]]
+    reference_image_id: Optional[Union[str, Any]]
+    lap: Optional[Union[str, Any]]
+    image: Optional[Union[str, Any]]
 
-    experimental: opt_int
-    suppressed_tail: opt_int
-    indoor: opt_int
-    hairless: opt_int
-    natural: opt_int
-    rare: opt_int
-    rex: opt_int
-    supress_tail: opt_int
-    short_legs: opt_int
-    hypoallergenic: opt_int
-    adaptability: opt_int
-    affection_level: opt_int
-    child_friendly: opt_int
-    dog_friendly: opt_int
-    stranger_friendly: opt_int
-    cat_friendly: opt_int
-    energy_level: opt_int
-    grooming: opt_int
-    health_issues: opt_int
-    intelligence: opt_int
-    shedding_level: opt_int
-    social_needs: opt_int
-    vocalisation: opt_int
-    bidability: opt_int
+    experimental: Optional[Union[int, Any]]
+    suppressed_tail: Optional[Union[int, Any]]
+    indoor: Optional[Union[int, Any]]
+    hairless: Optional[Union[int, Any]]
+    natural: Optional[Union[int, Any]]
+    rare: Optional[Union[int, Any]]
+    rex: Optional[Union[int, Any]]
+    supress_tail: Optional[Union[int, Any]]
+    short_legs: Optional[Union[int, Any]]
+    hypoallergenic: Optional[Union[int, Any]]
+    adaptability: Optional[Union[int, Any]]
+    affection_level: Optional[Union[int, Any]]
+    child_friendly: Optional[Union[int, Any]]
+    dog_friendly: Optional[Union[int, Any]]
+    stranger_friendly: Optional[Union[int, Any]]
+    cat_friendly: Optional[Union[int, Any]]
+    energy_level: Optional[Union[int, Any]]
+    grooming: Optional[Union[int, Any]]
+    health_issues: Optional[Union[int, Any]]
+    intelligence: Optional[Union[int, Any]]
+    shedding_level: Optional[Union[int, Any]]
+    social_needs: Optional[Union[int, Any]]
+    vocalisation: Optional[Union[int, Any]]
+    bidability: Optional[Union[int, Any]]
 
 
 class Category(BaseModel):
 
-    id: int
-    name: str
+    id: Union[int, Any]
+    name: Union[str, Any]
 
 
 class Vote(BaseModel):
 
-    id: str
-    image_id: str
-    sub_id: opt_str
-    created_at: str
-    value: int
-    country_code: opt_str
-    user_id: opt_str
+    id: Union[str, Any]
+    image_id: Union[str, Any]
+    sub_id: Optional[Union[str, Any]]
+    created_at: Union[str, Any]
+    value: Union[int, Any]
+    country_code: Optional[Union[str, Any]]
+    user_id: Optional[Union[str, Any]]
 
 
 class Response(BaseModel):
 
     id: Union[str, int, None]
-    message: str
-    level: opt_str
-    status: opt_int
+    message: Union[str, Any]
+    level: Optional[Union[str, Any]]
+    status: Optional[Union[int, Any]]
 
 
 class FavouriteImage(BaseModel):
 
-    id: str
-    url: str
+    id: Union[str, Any]
+    url: Union[str, Any]
 
 
 class Favourite(BaseModel):
 
-    id: str
-    image_id: str
-    sub_id: str
-    created_at: str
+    id: Union[str, Any]
+    image_id: Union[str, Any]
+    sub_id: Union[str, Any]
+    created_at: Union[str, Any]
     image: FavouriteImage
-    user_id: str
+    user_id: Union[str, Any]
 
     def __post_init__(self):
         self.image = FavouriteImage(**self.image)
@@ -98,23 +95,24 @@ class Favourite(BaseModel):
 
 class Image(BaseModel):
 
-    id: str
-    url: str
-    width: int
-    height: int
-    created_at: str
-    sub_id: str
-    original_filename: str
+    id: Union[str, Any]
+    url: Union[str, Any]
+    width: Union[int, Any]
+    height: Union[int, Any]
+    created_at: Union[str, Any]
+    sub_id: Union[str, Any]
+    original_filename: Union[str, Any]
 
     breeds: Breed
     categories: Category
 
 
+
 class Analysis(BaseModel):
 
-    image_id: str
+    image_id: Union[str, Any]
     labels: list[dict]
     moderation_labels: list[dict]
-    vendor: str
-    approved: int
-    rejected: int
+    vendor: Union[str, Any]
+    approved: Union[int, Any]
+    rejected: Union[int, Any]
